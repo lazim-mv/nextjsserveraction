352d4e54eaffefa05fffb5601aebd24b71f8b0a7
lazim-mv <106281909+lazim-mv@users.noreply.github.com>
2023-12-13 02:23:03 +0530

    first commit

---
+++ components/MotionDiv.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { motion } from "framer-motion";
+
+export const MotionDiv = motion.div;
+
+// function MotionDiv() {
+//   return (
+//     <motion.div
+//       className="max-w-sm rounded relative w-full"
+//       variants={variants}
+//       initial="hidden"
+//       animate="visible"
+//       transition={{
+//         delay: 1,
+//         ease: "easeInOut",
+//         duration: 0.5,
+//       }}
+//       viewport={{ amount: 0 }}
+//     ></motion.div>
+//   );
+// }
+
+// export default MotionDiv;
