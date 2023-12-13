36fdcb049bf15c0d32c1bf2b61f6c1f2b66ef55b
lazim-mv <106281909+lazim-mv@users.noreply.github.com>
2023-12-13 07:07:04 +0530

    v1.0.1

--- app/action.tsx
+++ app/action.tsx
@@ -2,13 +2,23 @@
 
 import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
 
-export const fetchAnime = async (page: number) => {
+export const fetchAnime = async (page: number): Promise<AnimeProp[]> => {
   const response = await fetch(
     `https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`
   );
 
   const data = await response.json();
-  return data.map((item: AnimeProp, index: number) => (
-    <AnimeCard key={item.id} anime={item} index={index} />
-  ));
+  console.log(data);
+  return data; // Assuming data is an array of AnimeProp objects
 };
+
+// export const fetchAnimeVideo = async (page: number) => {
+//   const response = await fetch(
+//     `https://shikimori.one//api/animes/:${}/roles`
+//   );
+
+//   const data = await response.json();
+//   return data.map((item: AnimeProp, index: number) => (
+//     <AnimeCard key={item.id} anime={item} index={index} />
+//   ));
+// };
--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -16,16 +16,18 @@ function LoadMore() {
   useEffect(() => {
     if (inView) {
       fetchAnime(page).then((res) => {
-        setData([...data, ...res]);
+        setData((prevData) => [...prevData, ...res]);
         page++;
       });
     }
-  }, [inView, data]);
+  }, [inView]);
 
   return (
     <>
       <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
-        {data}
+        {data.map((item, index) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
+        ))}
       </section>
       <section className="flex justify-center items-center w-full">
         <div ref={ref}>
